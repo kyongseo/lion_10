bdb0e42f7afaeb370916d5069e56de925c6de9f3
kyongseo <pokj930@naver.com>
2024-03-26 14:16:39 +0900

    day06

---
+++ src/main/java/org/example/day06/Car.java
@@ -0,0 +1,14 @@
+package org.example.day06;
+
+public class Car {
+    String name;
+    int speed;
+
+//    Car(){
+//        System.out.println(" Car() 생성자 호출됨.");
+//    }
+
+    public Car(String name){
+        this.name = name;
+    }
+}
