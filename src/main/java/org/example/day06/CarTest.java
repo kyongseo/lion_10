4af6a5f727950ab97d967ceaf42302f1c37ad239
kyongseo <pokj930@naver.com>
2024-03-26 14:16:43 +0900

    day06

---
+++ src/main/java/org/example/day06/CarTest.java
@@ -0,0 +1,11 @@
+package org.example.day06;
+
+import javax.xml.namespace.QName;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        Car car = new Car("BMW");
+        System.out.println("출력" + car.name);
+    }
+}
