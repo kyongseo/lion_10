8496a31952d06daf71e3f220da3a5efe2d6e170b
kyongseo <pokj930@naver.com>
2024-04-14 22:10:40 +0900

    static

---
+++ src/main/java/org/example/day09/SoojebiMain.java
@@ -0,0 +1,17 @@
+package org.example.day09;
+
+
+public class SoojebiMain {
+    public static void main(String[] args) {
+        Soojebi s = new Soojebi();
+        s.count++;
+        System.out.println(s.count);
+        s.count++;
+        System.out.println(s.count);
+
+//        Soojebi.count++;
+//        System.out.println(Soojebi.count);
+//
+//        Soojebi.print();
+    }
+}
