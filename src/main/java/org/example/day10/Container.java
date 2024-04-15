f46cf017e8d73c3fce97a581702f41ba0a430006
kyongseo <pokj930@naver.com>
2024-04-15 16:38:18 +0900

    Generic

---
+++ src/main/java/org/example/day10/Container.java
@@ -0,0 +1,6 @@
+package org.example.day10;
+
+public interface Container <T> {
+    void set (T value);
+    T get();
+}
---
+++ src/main/java/org/example/day10/StringContainer.java
@@ -0,0 +1,15 @@
+package org.example.day10;
+
+public class StringContainer implements Container<String> {
+    private String value;
+
+    @Override
+    public void set(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+}
