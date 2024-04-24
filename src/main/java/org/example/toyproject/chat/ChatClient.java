324b95bb0774e08a93f2bdf6ae20a18b85ab541a
kyongseo <pokj930@naver.com>
2024-04-24 16:57:58 +0900

    채팅프로젝트 - 멀티 스레드 구현

--- src/main/java/org/example/toyproject/chat/ChatClient.java
+++ src/main/java/org/example/toyproject/chat/ChatClient.java
@@ -1,68 +1,74 @@
 package org.example.toyproject.chat;
 
+import java.net.*;
 import java.io.*;
-import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatClient {
-    public static void main(String[] args) {
-        // 2. id 입력하지 않았다면 클라이언트 연결하지 않고 프로세스 종료
-        if (args.length != 1) {
-            System.out.println("사용법 : java ChatClent id");
-            System.exit(1);
+    public static void main(String args[]) {
+        if(args.length!=1) {
+            System.out.println("USAGE: java ChatClient 대화명");
+            System.exit(0);
         }
 
-        // 3. 서버 소켓과 동일한 포트로 연결하고, 입력 출력 객체 생성
-        try (Socket socket = new Socket("127.0.0.1", 9999);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-        ) {
-
-            // 7. 서버에게 id 전송
-            out.println(args[0]);
-
-            // 8. 쓰레드 시작
-            new InputThread(socket, in).start();
-
-            // 9. 클라이언트가 서버에게 입력값 전달(종료할 때까지 반복)
-            String msg = null;
-            while ((msg = keyboard.readLine()) != null) {
-                out.println(msg);
-                if ("/quit".equalsIgnoreCase(msg))
-                    break;
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-}
-
-class InputThread extends Thread {
-    private Socket socket;
-    private BufferedReader in;
-
-    InputThread(Socket socket, BufferedReader in) {
-        this.socket = socket;
-        this.in = in;
-    }
-
-    @Override
-    public void run() {
         try {
-            String msg = null;
-            while ((msg = in.readLine()) != null) {
-                System.out.println(msg);
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            String serverIp = "127.0.0.1";
+            // 소켓을 생성하여 연결 요청
+            Socket socket = new Socket(serverIp, 9999);
+            System.out.println("서버에 연결되었습니다.");
+            Thread sender   = new Thread(new ClientSender(socket, args[0]));
+            Thread receiver = new Thread(new ClientReceiver(socket));
+
+            sender.start();
+            receiver.start();
+        } catch(ConnectException ce) {
+            ce.printStackTrace();
+        } catch(Exception e) {}
+    } // main
+
+    static class ClientSender extends Thread {
+        Socket socket;
+        DataOutputStream out;
+        String name;
+
+        ClientSender(Socket socket, String name) {
+            this.socket = socket;
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                this.name = name;
+            } catch(Exception e) {}
         }
-    }
-}
+
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                if(out!=null) {
+                    out.writeUTF(name);
+                }
+
+                while(out!=null) {
+                    out.writeUTF("["+name+"] "+scanner.nextLine());					}
+            } catch(IOException e) {}
+        } // run()
+    } // ClientSender
+
+    static class ClientReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+
+        ClientReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+            } catch(IOException e) {}
+        }
+
+        public void run() {
+            while(in!=null) {
+                try {
+                    System.out.println(in.readUTF());
+                } catch(IOException e) {}
+            }
+        } // run
+    } // ClientReceiver
+} // class
--- src/main/java/org/example/toyproject/chat/ChatServer.java
+++ src/main/java/org/example/toyproject/chat/ChatServer.java
@@ -1,138 +1,83 @@
 package org.example.toyproject.chat;
 
-import java.io.*;
 import java.net.*;
+import java.io.*;
 import java.util.*;
 
 public class ChatServer {
-    public static void main(String[] args) {
-        // 1. 서버 소켓 설정 및 공유 객체 정의
-        try (ServerSocket serverSocket = new ServerSocket(9999);) {
-            System.out.println("서버가 준비되었습니다.");
-            Map<String, PrintWriter> chatClients = new HashMap<>();
+    HashMap clients;
 
-            while (true) {
-                // 4. 어떤 클라이언트가 연결을 시도하면 수락
-                Socket socket = serverSocket.accept();
-                // 5. 쓰레드 호출
-                new ChatThread(socket, chatClients).start();
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-            e.printStackTrace();
-        }
+    ChatServer() {
+        clients = new HashMap();
+        Collections.synchronizedMap(clients);
     }
-}
 
-class ChatThread extends Thread {
-    private Socket socket;
-    private String id;
-    private Map<String, PrintWriter> chatClients;
-
-    private BufferedReader in;
-    PrintWriter out;
-
-    // 6. 쓰레드가 호출되면 동시에 생성자 실행
-    public ChatThread(Socket socket, Map<String, PrintWriter> chatClients) {
-        this.socket = socket;
-        this.chatClients = chatClients;
+    public void start() {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
 
         try {
-            // 8. 클라이언트에게 id를 입력받고, broadcast를 통해 현재 연결되어 있는 클라이언트에게 입장 알림을 보내주고, 서버에 새로운 id를 출력한다.
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            id = in.readLine();
-            broadcast(id + "님이 입장하셨습니다.");
-            System.out.println("새로운 사용자의 아이디는 " + id + "입니다.");
+            serverSocket = new ServerSocket(9999);
+            System.out.println("서버가 시작되었습니다");
 
-            // 9. chatClients.put(this.id, out)를 실행할 때
-            // synchronized를 이용해 chatClients에 다른 쓰레드가 접근할 수 없게 해준다.
-            synchronized (chatClients) {
-                chatClients.put(this.id, out);
+            while(true) {
+                socket = serverSocket.accept();
+               // System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속하였습니다.");
+                ServerReceiver thread = new ServerReceiver(socket);
+                thread.start();
             }
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch(Exception e) {
             e.printStackTrace();
         }
+    } // start()
+
+    void sendToAll(String msg) {
+        Iterator it = clients.keySet().iterator();
+
+        while(it.hasNext()) {
+            try {
+                DataOutputStream out = (DataOutputStream)clients.get(it.next());
+                out.writeUTF(msg);
+            } catch(IOException e){}
+        } // while
+    } // sendToAll
+
+    public static void main(String args[]) {
+        new ChatServer().start();
     }
+    class ServerReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+        DataOutputStream out;
 
-    @Override
-    public void run() {
-        // 해당 클라이언트의 채팅이 시작됐다는 알림을 서버에게 전송하고,
-        // 클라이언트가 /quit를 입력하기 전까지 입력하는 값들은 전부 모든 클라이언트에게 전송해준다.
-        // 종료될 때까지 반복
-        System.out.println(id + "사용자 채팅시작!!");
-        String msg = null;
-        try {
-            while ((msg = in.readLine()) != null) {
-                if ("/quit".equalsIgnoreCase(msg))  break;
-
-//                if(msg.split(" ").length == 3 && "/to".equalsIgnoreCase(msg.split(" ")[0])){
-                if("/to".equalsIgnoreCase(msg.split(" ")[0])){
-                    sendMsg(id, msg.split(" ")[1], msg.split(" ")[2]);
-                } else{
-                    broadcast(id + " : " + msg);
-                }
-            }
-
-        } catch (IOException e) {
-            System.out.println(e);
-            e.printStackTrace();
-            // 해당 클라이언트가 /quit를 눌러 입력이 종료되면, 그 클라이언트를 공유객체에서 제거해주고,
-            // 모든 클라이언트에게 해당 클라이언트가 나갔다는 알림을 전송한다.
-        } finally {
-            synchronized (chatClients) {
-                chatClients.remove(id);
-            }
-            broadcast(id + "님이 채팅에서 나갔습니다.");
-
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        ServerReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in  = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+            } catch(IOException e) {}
         }
-    }
 
+        public void run() {
+            String name = "";
+            try {
+                name = in.readUTF();
+                sendToAll(name+"님이 들어왔습니다.");
 
-    public void broadcast(String msg) {
-        // broadcast에서 chatClients에 접근하는 동안엔 chatClients에 클라이언트 추가/제거가 불가하게 설정
-        synchronized (chatClients) {
-            // chatClients의 value값들을 저장한 it 객체 생성 즉, 연결된 모든 클라이언트가 저장됨
-            Iterator<PrintWriter> it = chatClients.values().iterator();
+                clients.put(name, out);
+                System.out.println("현재 서버의 접속자는 "+ clients.size()+"명 입니다.");
 
-            while (it.hasNext()) {
-                // 해당 클라이언트로 전송하는 out 객체 받아주기
-                PrintWriter out = it.next();
-                try {
-                    // 해당 클라이언트에게 전송
-                    out.println(msg);
-                } catch (Exception e) {
-                    // 뭔가 오류가 나면 해당 객체 삭제(chatClients에서도 삭제)
-                    it.remove();
-                    e.printStackTrace();
+                while(in!=null) {
+                    sendToAll(in.readUTF());
                 }
-            }
-        }
-    }
-
-    public void sendMsg(String fromId, String toId, String msg) {
-        synchronized (chatClients) {
-            if(chatClients.containsKey(toId)){
-                chatClients.get(toId).println(fromId + "님이 " + toId + "님에게 보낸 메시지: " + msg);
-            } else{
-                chatClients.get(fromId).println("해당 사용자가 존재하지 않습니다.");
-            }
-        }
-    }
-}
+            } catch(IOException e) {
+                // ignore
+            } finally {
+                sendToAll(name+"님이 나가셨습니다.");
+                clients.remove(name);
+                System.out.println("["+socket.getInetAddress() +":"+socket.getPort()+"]"+"에서 접속을 종료하였습니다.");
+                System.out.println("현재 서버의 접속자 수는 "+ clients.size()+"명 입니다.");
+            } // try
+        } // run
+    } // ReceiverThread
+} // class
