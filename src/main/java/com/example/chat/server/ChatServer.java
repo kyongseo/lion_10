3cdf885a2a65828cc7234cda4e92ed584b9013aa
kyongseo <pokj930@naver.com>
2024-04-27 18:35:37 +0900

    add : 서버 로직

--- src/main/java/com/example/chat/server/ChatServer.java
+++ src/main/java/com/example/chat/server/ChatServer.java
@@ -11,15 +11,19 @@ public class ChatServer {
         try (ServerSocket serverSocket = new ServerSocket(12345);) {
             System.out.println("서버가 준비되었습니다.");
 
-            // 클라이언트의 정보를 넣을 자료구조
+            // 클라이언트의 정보를 넣을 객체
+            // ==> 각 클라이언트의 닉네임, PrintWriter 저장
             Map<String, PrintWriter> clients = new HashMap<>();
 
-            // 사용자에 해당하는 방 정보를 넣을 자료구조
+            // 사용자에 해당하는 방 정보를 넣을 객체
+            // => 각 클라이언트의 닉네임, 해당하는 방 번호 저장
             Map<String, Integer> userRooms = new HashMap<>();
 
             while (true) {
+                // 클라이언트의 접속 대기, 클라이언트 소켓 받아오기
                 Socket clientSocket = serverSocket.accept();
 
+                // 새로운 클라이언트를 처리할 ChatServerThread 스레드
                 new ChatServerThread(clientSocket, clients, userRooms).start();
             }
 
