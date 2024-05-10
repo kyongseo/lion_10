2a59e8ee076bac60d3624529ab874aeae9f188d3
kyongseo <pokj930@naver.com>
2024-05-10 11:27:05 +0900

    bean

---
+++ src/main/java/sample/run/SpringExam1.java
@@ -0,0 +1,83 @@
+package sample.run;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import sample.bean.MyBean;
+import sample.config.MyBeanConfig;
+
+// 빈을 생성하는 하는 방법
+// 1. 직접 사용 -> 인스턴스 객체 생성하기
+// 2. 스프링 ioc 컨테이너 사용하기
+
+
+public class SpringExam1 {
+
+    public static void main(String[] args) {
+        /**
+         * 1. 직접 사용하는 경우
+         */
+        MyBean bean = new MyBean();
+        bean.setName("park");
+
+        System.out.println(bean);
+
+        /**
+         * 2. 스프링 ioc container 를 사용하는 경우
+         *  2-1) 스프링 컨포넌트 애노테이션으로 빈으로 사용할 것이라고 알려줘야 빈으로 등록 가능 -> 하지만 컴포넌트 스캔은 org.. 폴더에 있어야 함
+         *  2-2) 자바 파일을 통해서 알려줌 -- config 라는 패키지를 만들어서 @Bean 애노테이션을 달아서 메서드 생성하여 빈 등록
+         */
+
+        // (1) BeanFactory -- 빈을 생성하는데 간단한 기능만 포함 -> aop 기술은 사용 불가
+
+        System.out.println("ApplicationContext 생성 전");
+        // (2) ApplicationContext 을 사용하여 빈 사용하기
+        // 공장이 생성될 때 어떤 빈을 생성해야 할지 알아야 함 -> MyBeanConfig.class에 있는 빈을 등록해달라고 하는 구문
+        ApplicationContext context = new AnnotationConfigApplicationContext(MyBeanConfig.class);
+
+        System.out.println("ApplicationContext 생성 후");
+        MyBean bean1 = (MyBean)context.getBean("myBean"); // 아이디를 알려줌
+
+        bean1.setName("ks");
+
+        System.out.println(bean1);
+
+        // 빈에 아이디가 아닌 타입을 넣어서 호츌하기
+        // MyBean bean2 = context.getBean("myBean.class"); // 해당 타입의 객체가 하나만 존재할 경우는 오류가 없음
+        // 빈이 2개 일때는 오류생성되어 아래처럼 작성
+        MyBean bean2 = context.getBean("myBean2", MyBean.class); // 아이디가 아닌 타입만 알려준 것 -> 즉, 유추해서 하나만 골라줌
+
+        bean2.setName("kim");
+        System.out.println(bean2);
+        System.out.println(bean1);
+
+        // == 은 주소, .equals은 값이 같은지..
+        if (bean2 == bean1) {
+            System.out.println("bean1과 bean2은 주소가 같아요. 같은 인스턴스입니다.");
+        } else {
+            System.out.println("bean1과 bean2는 다른 인스턴스입니다.");
+        }
+
+        MyBean bean3 = context.getBean("myBean", MyBean.class);
+        if (bean3 == bean1) {
+            System.out.println("bean1과 bean3은 같은 인스턴스입니다.");
+        }
+
+        // @Scope("prototype") 으로 생성하기
+
+        System.out.println("============@Scope 생성하기============");
+
+        MyBean bean4 = context.getBean("myBean3", MyBean.class);
+        bean4.setName("hong");
+
+        MyBean bean5 = context.getBean("myBean3", MyBean.class);
+        bean5.setName("lee");
+
+        System.out.println(bean4);
+        System.out.println(bean5);
+
+         /**
+          *   2-3) xml을 통해서 알려줌 -> <bean id = "???" class "sample.MyBean"/> 으로 등록하여 사용 -- (현재는 사용하지 않음)
+         */
+
+    }
+}
