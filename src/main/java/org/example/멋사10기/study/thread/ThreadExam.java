00f0f0c91e4c73d475aa8ce669fd8d26e898fd7d
kyongseo <pokj930@naver.com>
2024-05-19 11:24:57 +0900

    java Thread

--- src/main/java/org/example/멋사10기/study/thread/ThreadExam.java
+++ src/main/java/org/example/멋사10기/study/thread/ThreadExam.java
@@ -2,11 +2,14 @@ package org.example.멋사10기.study.thread;
 
 /**
  * 쓰레드 만들기(extend Thread)
- * 자바에서 Thread를 만드는 방법은 크게 Thread 클래스를 상속받는 방법과 Runnable인터페이스를 구현하는 방법이 있다.
- * Thread를 상속 받아서 쓰레드를 생성하는 방법
+ * 자바에서 Thread를 만드는 방법은 크게
+ * (1) Thread 클래스를 상속받는 방법과
+ * (2) Runnable인터페이스를 구현하는 방법이 있다.
+
+ * (1) Thread를 상속 받아서 쓰레드를 생성하는 방법
  * java.lang.Thread클래스를 상속받는다. 그리고 Thread가 가지고 있는 run()메소드를 오버라이딩한다.
  * 10번 반복하면서 str을 찍습니다.
- *
+
  * Thread 클래스를 상속받은 MyThread1을 사용하는 클래스
  * Thread를 상속 받았으므로 MyThread1은 Thread 이다.
  * 쓰레드를 생성하고, Thread 클래스가 가지고 있는 start() 메소드를 호출 한다.
--- src/main/java/org/example/멋사10기/study/thread/ThreadExam2.java
+++ src/main/java/org/example/멋사10기/study/thread/ThreadExam2.java
@@ -2,10 +2,13 @@ package org.example.멋사10기.study.thread;
 
 /**
  * 쓰레드 만들기(implements Runnable)
- * 자바에서 Thread를 만드는 방법은 크게 Thread 클래스를 상속받는 방법과 Runnable인터페이스를 구현하는 방법이 있다.
- * Runnable인터페이스를 구현해서 쓰레드를 만드는 방법
+ * 자바에서 Thread를 만드는 방법은 크게
+ * (1) Thread 클래스를 상속받는 방법과
+ * (2) Runnable인터페이스를 구현하는 방법이 있다.
+
+ * (2) Runnable인터페이스를 구현해서 쓰레드를 만드는 방법
  * Runable 인터페이스가 가지고 있는 run()메소드를 구현한다.
- *
+
  * Runable 인터페이스를 구현한 MyThread2 사용하는 방법
  * MyThread2는 Thread를 상속받지 않았기 때문에 Thread가 아니다.
  * Thread를 생성하고, 해당 생성자에 MyThread2를 넣어서 Thread를 생성한다.
